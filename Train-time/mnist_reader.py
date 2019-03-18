6bee30d204153e27748f42d413443c1f995b8754
David Peter <dpet@mailbox.org>
2019-03-18 13:57:57 +0100

    Add structured label noise

--- Train-time/mnist_reader.py
+++ Train-time/mnist_reader.py
@@ -17,6 +17,12 @@ class MnistReader(object):
         self._validation_data = validation_data
         self._test_data = test_data
 
+        train_X, train_y = training_data
+        self._train_samples = {i: None for i in range(10)}
+        for i in range(10):
+            train_i = np.where(train_y == i)[0]
+            self._train_samples[i] = train_X[train_i, :]
+
     def get_train_data(self, n_samples=None, noise=None, alpha=0, delta=0):
         if not n_samples:
             n_samples = self._training_data[0].shape[0]
@@ -35,10 +41,45 @@ class MnistReader(object):
 
             train_X = np.concatenate((clean_X, noisy_X))
             train_y = np.concatenate((clean_y, noisy_y))
-            #print(train_X.shape)
-            #print(train_y.shape)
 
             return train_X, train_y
+        elif noise == 's':
+            confusion_matrix = np.loadtxt("./results/confusion.dat")
+
+            similarities = np.argsort(confusion_matrix, axis=1)
+            similarities = np.flip(similarities, axis=1)
+
+            deltas = delta * (-np.arange(0, 10, 1) + 5) / 4
+            probabilities = 1 / 10 + deltas / (1 + alpha)
+            probabilities[0] = 1 / 10
+
+            clean_X = self._training_data[0][0:n_samples]
+            clean_y = self._training_data[1][0:n_samples]
+
+            # For every clean sample with label i, add alpha samples with label i and random label from 0 to 9
+            train_X = clean_X
+            train_y = clean_y
+            # real_train_y = clean_y
+            for i in range(10):
+                # First, get number of samples with label i
+                num_samples = np.count_nonzero(clean_y == i)
+                num_noisy = num_samples * alpha
+
+                # Get num_noisy samples
+                sample_indices = np.random.randint(0, high=self._train_samples[i].shape[0], size=num_noisy)
+                noisy_X = self._train_samples[i][sample_indices, :]
+
+                # Labels are randomly assigned according to probability distribution given by the delta factor
+                p = np.zeros(probabilities.shape)
+                p[similarities[i]] = probabilities
+                noisy_y = np.random.choice(10, size=num_noisy, p=p)
+
+                train_X = np.concatenate((train_X, noisy_X))
+                train_y = np.concatenate((train_y, noisy_y))
+
+                # real_train_y = np.concatenate((real_train_y, np.ones(num_noisy) * i))
+
+            return train_X, train_y #, real_train_y
 
         return None
 
