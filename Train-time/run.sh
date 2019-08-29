9b996f3e1fbe31ae93089d12a931c900b94ff210
David Peter <deekay@gpu1.spsc.tugraz.at>
2019-08-29 19:43:51 +0200

    Add performance data

--- Train-time/run.sh
+++ Train-time/run.sh
@@ -4,12 +4,14 @@
 #SBATCH --output=./slurm_out/nn_%A_%a.out
 #SBATCH --error=./slurm_out/nn_%A_%a.err
 #SBATCH --export=HOME=/clusterFS/home/student/deekay,PATH
-#SBATCH --time=02:00:00
+#SBATCH --time=04:00:00
 #SBATCH --partition=gpu,gpu2,gpu6
-#SBATCH --gres=gpu:GeForceGTX1080Ti:1
-#SBATCH --mem 4G
+#SBATCH --array=50
+#SBATCH --gres=gpu:TeslaK40c:1
+#SBATCH --mem 8G
+#SBATCH --cpus-per-gpu=4
 
 source $HOME/.bashrc
 conda activate theano
-python mnist.py --binary -a 20
+python mnist.py --binary -a ${SLURM_ARRAY_TASK_ID}
 
