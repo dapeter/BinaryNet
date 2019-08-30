b10ca41afe747f2fbdc9fb7fc18635f1aded036f
David Peter <deekay@gpu1.spsc.tugraz.at>
2019-08-30 17:50:18 +0200

    Add performance data for two layer mlp

--- Train-time/run.sh
+++ Train-time/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=b
+#SBATCH --job-name=mlp2nb
+#SBATCH --output=./slurm_out/mlp2nb_%A_%a.out
+#SBATCH --error=./slurm_out/mlp2nb_%A_%a.err
 #SBATCH --workdir=/clusterFS/home/student/deekay/BinaryNet/Train-time
-#SBATCH --output=./slurm_out/b_%A_%a.out
-#SBATCH --error=./slurm_out/b_%A_%a.err
 #SBATCH --export=HOME=/clusterFS/home/student/deekay,PATH
 #SBATCH --time=04:00:00
 #SBATCH --partition=gpu,gpu2,gpu6
