c60970dda5f44a149a95c27b190834d92c6ba53a
David Peter <deekay@gpu1.spsc.tugraz.at>
2019-08-30 14:10:41 +0200

    New data

--- Train-time/run.sh
+++ Train-time/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=nn
+#SBATCH --job-name=b
 #SBATCH --workdir=/clusterFS/home/student/deekay/BinaryNet/Train-time
-#SBATCH --output=./slurm_out/nn_%A_%a.out
-#SBATCH --error=./slurm_out/nn_%A_%a.err
+#SBATCH --output=./slurm_out/b_%A_%a.out
+#SBATCH --error=./slurm_out/b_%A_%a.err
 #SBATCH --export=HOME=/clusterFS/home/student/deekay,PATH
 #SBATCH --time=04:00:00
 #SBATCH --partition=gpu,gpu2,gpu6
-#SBATCH --array=0-50:5%2
+#SBATCH --array=0-50:10%4
 #SBATCH --gres=gpu:TeslaK40c:1
 #SBATCH --mem 6G
-#SBATCH --cpus-per-gpu=4
+#SBATCH --cpus-per-gpu=2
 
 source $HOME/.bashrc
 conda activate theano
