5bb44462518bc68d86baa56a555e89d1dbf2945e
David Peter <deekay@gpu1.spsc.tugraz.at>
2019-09-09 14:47:34 +0200

    Add most recent sbatch file

--- Train-time/run.sh
+++ Train-time/run.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=cnn4b
-#SBATCH --output=./slurm_out/cnn4b_%A_%a.out
-#SBATCH --error=./slurm_out/cnn4b_%A_%a.err
+#SBATCH --job-name=nconv4b
+#SBATCH --output=./slurm_out/nconv4b_%A_%a.out
+#SBATCH --error=./slurm_out/nconv4b_%A_%a.err
 #SBATCH --workdir=/clusterFS/home/student/deekay/BinaryNet/Train-time
 #SBATCH --export=HOME=/clusterFS/home/student/deekay,PATH
-#SBATCH --time=04:00:00
 #SBATCH --partition=gpu,gpu2,gpu6
-#SBATCH --array=0-50:10%2
+#SBATCH --array=1-10:2%2
 #SBATCH --gres=gpu:TeslaK40c:1
-#SBATCH --mem 6G
-#SBATCH --cpus-per-gpu=2
+#SBATCH --mem=16
 
 source $HOME/.bashrc
 conda activate theano
-python mnist_cnn.py --binary -a ${SLURM_ARRAY_TASK_ID}
+python cifar10_big.py --binary -a ${SLURM_ARRAY_TASK_ID}
 
